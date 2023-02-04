130415171afb66a14a867557ec007a34e6192271
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-02-04 00:46:57 +0000

    main layout and styling finished and to do list is working and functional

---
+++ script.js
@@ -0,0 +1,30 @@
+let addToDoButton = document.getElementById('toDo');
+let toDoContainer = document.getElementById('container');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function() {
+    var paragraph = document.createElement('p');
+    var removeButton = document.createElement('button');
+    var completeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    completeButton.innerText = 'Complete';
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    toDoContainer.appendChild(removeButton);
+    toDoContainer.appendChild(completeButton);
+    removeButton.classList.add('remove-button');
+    completeButton.classList.add('complete-button');
+    inputField.value = '';
+    completeButton.addEventListener('click', function() {
+        paragraph.style.textDecoration = 'line-through';
+    })
+    
+    
+    
+    
+    removeButton.addEventListener('click', function() {
+        toDoContainer.removeChild(paragraph);
+        toDoContainer.removeChild(removeButton);
+        toDoContainer.removeChild(completeButton);
+    })
+})
