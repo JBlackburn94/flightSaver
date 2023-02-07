62220f660b46d943c0150bcc8c60fcb9c17e437a
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-02-07 15:23:03 +0000

    fixed some responsive margin issues

--- script.js
+++ script.js
@@ -6,21 +6,22 @@ addToDoButton.addEventListener('click', function() {
     var paragraph = document.createElement('p');
     var removeButton = document.createElement('button');
     var completeButton = document.createElement('button');
+    
     removeButton.innerText = 'Remove';
     completeButton.innerText = 'Complete';
     paragraph.innerText = inputField.value;
+    
     toDoContainer.appendChild(paragraph);
     toDoContainer.appendChild(removeButton);
     toDoContainer.appendChild(completeButton);
+    
     removeButton.classList.add('remove-button');
     completeButton.classList.add('complete-button');
     inputField.value = '';
+    
     completeButton.addEventListener('click', function() {
         paragraph.style.textDecoration = 'line-through';
     })
-    
-    
-    
     
     removeButton.addEventListener('click', function() {
         toDoContainer.removeChild(paragraph);
